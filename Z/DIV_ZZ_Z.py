2e213f4e18f519b1b57fd239f7636c09988ab89c
ArtemSokolovsky <artem.soki@yandex.ru>
2025-11-07 00:08:49 +0300

    Sokolovskii_Artem_Z

--- Z/DIV_ZZ_Z.py
+++ Z/DIV_ZZ_Z.py
@@ -1,15 +1,19 @@
 def __truediv__(self, other):
     """
     Сделал: Соколовский Артём
-    Деление целых чисел: self / other (целая часть).
+    Деление целых чисел: self / other.
     """
     if all(d == 0 for d in other.A):
         raise ZeroDivisionError("DIV_ZZ_Z: деление на ноль")
 
     A = int(''.join(map(str, self.A))) if self.A else 0
     B = int(''.join(map(str, other.A))) if other.A else 0
+
     q_abs = abs(A) // abs(B)
     result_sign = 1 if self.s != other.s else 0
 
+    if q_abs == 0:
+        return Integer(0, 0, [0])
+
     digits = [int(ch) for ch in str(q_abs)]
-    return {"sign": result_sign, "digits": digits}
+    return Integer(result_sign, len(digits) - 1, digits)
--- Z/MUL_ZZ_Z.py
+++ Z/MUL_ZZ_Z.py
@@ -4,9 +4,13 @@ def __mul__(self, other):
     Умножение целых чисел: self * other.
     """
     result_sign = 1 if self.s != other.s else 0
+
     A = int(''.join(map(str, self.A))) if self.A else 0
     B = int(''.join(map(str, other.A))) if other.A else 0
     product = A * B
 
+    if product == 0:
+        return Integer(0, 0, [0])
+
     digits = [int(ch) for ch in str(abs(product))]
-    return {"sign": result_sign, "digits": digits}
+    return Integer(result_sign, len(digits) - 1, digits)
