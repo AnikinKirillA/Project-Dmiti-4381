4c8d98fe5c61d54bc8a2887072be84ef970c4a99
npimkhovik_24 <imknatalia@mail.ru>
2025-11-07 19:01:51 +0300

    Q: ADD_QQ_Q self added

--- Q/ADD_QQ_Q.py
+++ Q/ADD_QQ_Q.py
@@ -1,18 +1,18 @@
-def ADD_QQ_Q(A: Rational, B: Rational) -> Rational:
+def ADD_QQ_Q(self, other):
     """
     Сделала: Имховик Наталья
     Выполняет сложение дробей
     Возвращает дробь
     """
     # Находим НОК знаменателей
-    lcm = LCM_NN_N(A.denominator, B.denominator)
+    lcm = LCM_NN_N(self.denominator, other.denominator)
 
     # Находим дополнительные множители
-    multiplyerA = lcm // A.denominator
-    multiplyerB = lcm // B.denominator
+    multiplyerA = lcm // self.denominator
+    multiplyerB = lcm // other.denominator
 
     # Умножаем числители на дополнительные множители и складываем
-    newNumerator = A.numerator * multiplyerA + B.numerator * multiplyerB
+    newNumerator = self.numerator * multiplyerA + other.numerator * multiplyerB
 
     # Создаем результирующую дробь
     return Rational(newNumerator, lcm)
