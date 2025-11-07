2bddbd8f1db3f5325b01efbd58409c488db3b303
npimkhovik_24 <imknatalia@mail.ru>
2025-11-07 18:54:54 +0300

    N: LCM_NN_N self added

--- N/LCM_NN_N.py
+++ N/LCM_NN_N.py
@@ -1,9 +1,9 @@
-def LCM_NN_N(A: Natural, B: Natural) -> Natural:
+def LCM_NN_N(self, other):
     """
     Сделала: Имховик Наталья
     Нахождение НОК натуральных чисел
     Используем НОК(a, b) = (a * b) / НОД(a, b)
     Возвращает натуральное
     """
-    gcd = GCF_NN_N(A, B)
-    return A * B // gcd
+    gcd = GCF_NN_N(self, other)
+    return self * other // gcd
