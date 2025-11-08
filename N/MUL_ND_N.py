500ec8e1431672a59d4234daf3a2d3d58c1fffe7
mvsurin_24 <uloxvk@gmail.com>
2025-11-08 13:06:44 +0300

    N fixed fixed

--- N/MUL_ND_N.py
+++ N/MUL_ND_N.py
@@ -4,12 +4,11 @@ def MUL_ND_N(self, int):
     Умножение натурального числа на цифру
     """
     
-    rev_num = self.A.reverse() # Запись числа справа налево
-
+    rev_num = self.A.copy()[::-1] # Запись числа справа налево
+    
     """ Домножение каждого разряда на цифру """
     for i in range(self.len + 1): 
         rev_num[i] *= int
-
 
     """ Перенос при переполнении разряда """
     for i in range(self.len + 1):
@@ -18,10 +17,10 @@ def MUL_ND_N(self, int):
             В текущем разряде сохраняем единицы,
             в следующий переносим десятки
             """
-            rev_num[i] %= 10
             if i < self.len:
-                rev_num[i+1] = rev_num[i] // 10
+                rev_num[i+1] += rev_num[i] // 10
             else: 
                 rev_num.append(rev_num[i] // 10)
+            rev_num[i] %= 10
     
-    return Natural(len(rev_num) - 1, rev_num.reverse())
+    return rev_num[::-1]
